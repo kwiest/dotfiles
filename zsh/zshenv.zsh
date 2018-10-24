90d6efa023f93a5f1457f1b1fd29fdef01398c98
Kyle Wiest <kyle.wiest@gmail.com>
2018-10-24 15:51:09 -0700

    Add paths for Homebrew, QT 5.5, and Postgres

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -1,2 +1,11 @@
+# Add Homebrew to PATH
+export PATH="/usr/local/bin:$PATH"
+
+# Add QT 5.5 to PATH
+export PATH="$(brew --prefix qt@5.5)/bin:$PATH"
+
+# Add Postgres to PATH
+export PATH="/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH"
+
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
